4b7224912cc68396ac9e8729d68f0d038f73c510
hassen <khelifi_hassen@yahoo.fr>
2016-05-01 19:04:56 +0200

    first commit

---
+++ web/src/main/java/com/formation/adhesion/ods/web/server/service/LivreServiceImpl.java
@@ -0,0 +1,56 @@
+package com.formation.adhesion.ods.web.server.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.formation.adhesion.ods.core.dao.LivreDao;
+import com.formation.adhesion.ods.core.model.Livre;
+import com.formation.adhesion.ods.web.shared.service.LivreService;
+
+public class LivreServiceImpl implements LivreService {
+
+	@Autowired
+	private LivreDao livreDao;
+	
+	protected LivreDao getLivreDao(){
+		return livreDao;
+	}
+
+	
+	
+
+	@Override
+	public List<Livre> getLivres() {
+
+		return livreDao.getLivres();
+	}
+
+	@Override
+	public Livre getLivre(long id) {
+
+		return livreDao.find(id);
+	}
+
+	public void setLivreDao(LivreDao livreDao) {
+		this.livreDao = livreDao;
+	}
+
+	public LivreDao getLivreDao() {
+		return livreDao;
+	}
+
+	@Override
+	public void deleteLivre(long id) {
+		livreDao.delete(id);
+	}
+
+	@Override
+	public void updateLivre(Livre livre) {
+		livreDao.merge(livre);
+		
+	}
+	
+	
+
+}
